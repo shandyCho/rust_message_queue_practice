c8176a4f90575f07e4e60f2981a1f50dde79da44
shandyCho <whqjarn111@gmail.com>
2025-12-24 16:57:39 +0900

    TCP 전송 에러 처리 로직 개선
    에러 발생시 panic 발생 시키는 대신 에러 정보 출력하도록 변경

--- src/serve_client/serve_client.rs
+++ src/serve_client/serve_client.rs
@@ -2,9 +2,27 @@ use std::net::TcpStream;
 use std::io::Write;
 use crate::handle_client::HttpRequestBody;
 
+// pub fn serve_client(request_body: HttpRequestBody<String>) {
+//     let mut stream = TcpStream::connect(request_body.get_sender_address())
+//         .expect("Could not connect to target address");
+//     stream.write_all(request_body.get_data().as_bytes())
+//         .expect("Failed to send data to target address");
+// }
+
 pub fn serve_client(request_body: HttpRequestBody) {
-    let mut stream = TcpStream::connect(request_body.get_sender_address())
-        .expect("Could not connect to target address");
-    stream.write_all(request_body.get_data().as_bytes())
-        .expect("Failed to send data to target address");
+    // let mut stream = TcpStream::connect(request_body.get_sender_address())
+    //     .expect("Could not connect to target address");
+    // stream.write_all(request_body.get_data().as_bytes())
+    //     .expect("Failed to send data to target address");
+
+    match TcpStream::connect::<&String>(request_body.get_sender_address()) {
+        Ok(mut stream) => {
+            println!("Connected to target address: {}", request_body.get_sender_address());
+            stream.write_all(request_body.get_data().as_bytes())
+                .expect("Failed to send data to target address");
+        }
+        Err(e) => {
+            eprintln!("Could not connect to target address: {}", e);
+        }
+    }
 }
