79f56620cbdf4400bc4be88f47d4cee52c725fb3
shandyCho <whqjarn111@gmail.com>
2026-02-11 14:40:56 +0900

    Tcp Listening Thread와 File Write & Message Queue Push Thread 분리

--- src/handle_client/handle_client.rs
+++ src/handle_client/handle_client.rs
@@ -1,10 +1,10 @@
 /// 메세지 큐는 따로 HTTP Body에 담긴 데이터를 처리할 필요가 없기 때문에 타겟으로 지정된 어드레스로 보내는 것만 신경 써볼것
 use core::str;
-use std::sync::Arc;
+use std::{net::SocketAddr};
 
 use serde::{Deserialize, Serialize};
 use serde_json::{Value, Error};
-use tokio::{io::AsyncWriteExt, net::{TcpListener, TcpSocket, TcpStream}, sync::mpsc};
+use tokio::{net::{TcpStream, tcp::{OwnedWriteHalf}}, sync::mpsc};
 use tokio_util::codec::{FramedRead, LinesCodec};
 use tokio_stream::StreamExt;
 
@@ -26,18 +26,26 @@ impl SubscribeMessage {
     }
 }
 
-pub async fn handle_connection(mut socket: TcpStream, tx: mpsc::UnboundedSender<Option<(TcpSocket, SubscribeMessage)>>) {
+
+pub async fn handle_connection(mut socket: TcpStream, addr: SocketAddr, tx: mpsc::UnboundedSender<Option<SubscribeMessage>>) {
     // 줄바꿈 문자 (\n) 기준으로 데이터를 읽어들이는 Reader 생성
     // LinesCodec 의 기본 설정이 줄바꿈 문자로 데이터를 구분하는 것임
-    let mut framed_reader = FramedRead::new(socket, LinesCodec::new());
-    while let Some(result) = framed_reader.next().await {
+    let (r, w) = socket.into_split();
+
+    let mut framed_reader = FramedRead::new(r, LinesCodec::new());
+    'frame_reading: while let Some(result) = framed_reader.next().await {
         match result {
             Ok(line) => {
                 let parsed: SubscribeMessage = serde_json::from_str::<SubscribeMessage>(&line)
                     .unwrap_or_else(|e: Error| {
                         eprintln!("Failed to parse JSON: {}", e);
                         eprintln!("Converted Data: {}", &line);
-                        tx.send((socket, None));
+                        send_error_to_client("Failed to parse request body.".to_string(),  &w, addr);
+                        
+                        let _tx_result = tx.send( None).unwrap_or_else(|e| {
+                            eprintln!("Fail to send message to another thread with e: {}", e);
+                        });
+                        
                         SubscribeMessage {
                             classifier: "".to_string(), 
                             data: serde_json::Value::String(String::new()) 
@@ -46,27 +54,36 @@ pub async fn handle_connection(mut socket: TcpStream, tx: mpsc::UnboundedSender<
                 
                 if !parsed.get_classifier().is_empty() {
                     println!("sender_address: {:?}, data: {:?}", &parsed.get_classifier(), &parsed.get_data());
-                    tx.send(Some(parsed));
+                    tx.send(Some(parsed))
+                    .unwrap_or_else(|e| {
+                        eprintln!("Parsing Messege is Success But fail to send message to another thread with e: {}", e);
+                    });
              
+                } else {
+                    // 메세지 파싱에서 에러가 발생할 경우, 반복문을 나와서 나머지 데이터들을 버려줄 필요가 있다
+                    println!("parsing error");
+                    break 'frame_reading;
                 }
             }
             Err(e) => {
                 eprintln!("Error reading line: {}", e);
-                tx.send(None);
+                tx.send((None)).unwrap_or_else(|e| {
+                    eprintln!("Fail to send message to another thread with LinesCodecError with e: {}", e);
+                });
+                break 'frame_reading;
             }
         };
     };
 }
 
-pub async fn send_error_to_client(err_msg: String, listner: Arc<TcpListener>) {
-
-    if let Ok(accept_result) = listner.accept().await {
-        let (mut socket, _addr) = accept_result;
-        socket
-            .write_all("Server can not parse client message".as_bytes())
-            .await
-            .inspect_err(|err| println!("Can not send error message to client: {}", err));
-    } else {
-            eprintln!("Failed to accept connection for sending error message: {}", err_msg);
+pub fn send_error_to_client(err_msg: String, mut socket: &OwnedWriteHalf, addr: SocketAddr) {
+    let write_result = socket.try_write(err_msg.as_bytes());
+    match write_result {
+        Ok(_) => eprintln!("Send error to client is Success. System Will close TcpStream from {}", addr),
+        Err(e) => eprintln!("Send error to client is failed with e: {}", e)
     };
+
+        
+        
+
 }
--- src/sub_and_pub/sub_and_pub.rs
+++ src/sub_and_pub/sub_and_pub.rs
@@ -1,6 +1,6 @@
 
-use std::{io::Error, path::PathBuf, sync::Arc, thread};
-use tokio::{net::{TcpListener, TcpSocket}, sync::mpsc};
+use std::{io::Error, net::SocketAddr, path::PathBuf, sync::Arc, thread};
+use tokio::{net::{TcpListener, TcpStream, tcp::WriteHalf}, sync::mpsc};
 
 use crate::{
     handle_client::{SubscribeMessage, handle_client},
@@ -18,44 +18,30 @@ use crate::{
 pub async fn sub_and_pub<T>(listner: TcpListener, file_path: PathBuf, mut message_queue: Vec<String>, mut message_store_vector: Vec<String>) -> Result<(), Error> {
     
     // clinet의 요청을 처리할 함수와 통신할 채널 생성
-    let (tx, mut rx) = mpsc::unbounded_channel::<(TcpSocket, Option<SubscribeMessage>)>();
-    let 
-    // Arc를 사용하여 여러군데에서 TcpListener를 사용하여 작업을 할 수 있도록 함?
-    let listner_arc = Arc::new(listner);
-    let listner_copy_for_listning = Arc::clone(&listner_arc).clone();
-    let listner_copy_for_writing = Arc::clone(&listner_arc).clone();
+    let (tx, mut rx) = mpsc::unbounded_channel::<Option<SubscribeMessage>>();
 
-    let t = thread::spawn(async move || {
+    let tcp_listening_thread = tokio::spawn(async move {
         // TCP/IP 연결 수신 루프
-            loop {
-                if let Ok(accept_result) = listner_copy_for_listning.accept().await {
-                    let (mut socket, addr) = accept_result;
-                    let tx_clone = tx.clone();
-                    println!("Stream loop entered");
-                    tokio::spawn(async move {
-                        handle_client::handle_connection(socket, tx_clone).await;
-                        println!("serve_client called");
-                    });
-                } else {
-                    eprintln!("Failed to accept connection");
-                }
-            };
+        loop {
+            if let Ok(accept_result) = listner.accept().await {
+                let (mut socket, addr) = accept_result;
+                let tx_clone = tx.clone();
+                println!("Stream loop entered");
+                handle_client::handle_connection(socket, addr, tx_clone).await;
+                println!("serve_client called");
+                
+                
+            } else {
+                eprintln!("Failed to accept connection");
+            }
+        };
     });
 
-    t.join();
-
     // 채널 수신 및 데이터 처리 로직 비동기 블럭 내에 생성
-    tokio::spawn(async move {
-        while let Some((socket, message)) = rx.recv().await {
+    let message_store_thread = tokio::spawn(async move {
+        while let Some(message) = rx.recv().await {
             if message.is_none() {
-                // 메세지가 없을 때에는 클라이언트에게 에러 메세지를 전송해줄 필요가 있음
-                // 그렇기 때문에 메세지를 받아온 곳에다가 에러 메세지를 보내고 나머지 데이터를 버리는 작업이 필요함
-                // let listner_copy = Arc::clone(&listner_arc);
                 eprintln!("Failed to parse request body. Skipping this connection.");
-                handle_client::send_error_to_client(
-                    "Failed to parse request body.".to_string(), 
-                    listner_copy_for_writing)
-                    .await;
                 continue;
             } else {
                 // TODO: 메세징 큐 변수로 데이터를 로드하는 로직이 필요함
@@ -63,7 +49,6 @@ pub async fn sub_and_pub<T>(listner: TcpListener, file_path: PathBuf, mut messag
                 let msg = message.unwrap();
                 message_queue.push(msg.get_data().clone());
                 message_store_vector.push(msg.get_data().clone());
-
 
                 println!("message_store_vector.len() == {}", message_store_vector.len());
                 if message_store_vector.len() == 5 {
@@ -85,5 +70,8 @@ pub async fn sub_and_pub<T>(listner: TcpListener, file_path: PathBuf, mut messag
         }
     });
 
+
+    let _result = tcp_listening_thread.await.unwrap_or_else(|f| {eprintln!("{}", f)});
+    let _result2 = message_store_thread.await.unwrap_or_else(|f| {eprintln!("{}", f)});
     Ok(())
 }
